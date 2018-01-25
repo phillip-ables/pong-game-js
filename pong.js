d91299d6efde2aebf15e99162f17c21d641da4cb
Phillip <phillipables1@gmail.com>
2018-01-25 17:20:00 -0500

    callback function that calls update with change of time perameters in s, stores lastTime var and calls requestAnimationFrame

--- pong.js
+++ pong.js
@@ -38,14 +38,27 @@ const context = canvas.getContext('2d');
 
 //we can use this ball as a basis for painting the ball
 const ball = new Ball;
+ball.vel.x = 150;
+ball.vel.y = 150;
+
+//request animation frame is a function that takes a callback and then calls the call back for the next time the browsers ready to draw
+//gives you elapsed time since the page was loaded
+let lastTime;
+function callback(millis) {
+  if (lastTime) {
+    update((millis - lastTime) / 1000);
+  }
+  lastTime = millis;
+  requestAnimationFrame(callback);
+}
 
 //animate the balls
 //update function that takes time
 function update(dt){
   // updates the balls position
   //position of ball is relative to the change in velocity
-  this.ball.pos.x += ball.vel.x * dt;
-  this.ball.pos.y += ball.vel.y * dt
+  ball.pos.x += ball.vel.x * dt;
+  ball.pos.y += ball.vel.y * dt;
 
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -54,3 +67,5 @@ function update(dt){
   context.fillStyle = "#fff";
   context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
 }
+
+callback();
