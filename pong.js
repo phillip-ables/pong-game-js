1d8943f1452714005e8dec71ecf97b6b950db866
Phillip <phillipables1@gmail.com>
2018-01-25 15:59:27 -0500

    hardcoded white ball in the upper left corner of the context

--- pong.js
+++ pong.js
@@ -1,5 +1,11 @@
+
+//we have a black rectangle canvas
 const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
 
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+//drawl Ball
+context.fillStyle = "#fff";
+context.fillRect(0, 0, 10, 10);
