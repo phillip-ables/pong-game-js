c0933f8a0c67b4b0c8ef0fc7760be3a7b307dfe6
Phillip <phillipables1@gmail.com>
2018-01-25 16:25:14 -0500

    Rect class that drawRect and a Ball class that extends and has a velocity

--- pong.js
+++ pong.js
@@ -1,14 +1,39 @@
 //bit hard coded
 //set up a vector class to hold x pos and y _lastPos
-class vector
+class Vec
 {
   constructor(x = 0, y = 0){
     this.x = x;
     this.y = y;
   }
 }
-//drawRect along so I want to generalize that
 
+//drawRect along so I want to generalize that
+class Rect
+{
+  //the variables it takes are extended from the other variables into these variables
+  constructor(w, h){
+    //all objects have a position
+    //position is a vector
+    this.pos = new Vec;
+    //use w and h for x and y because this actually is width and height
+    this.size = new Vec(w, h);
+  }
+}
+
+//ball drawRect class
+//extend and super
+class Ball extends Rect
+{
+  constructor(){
+    //all balls are a rec and have speed
+    super(10, 10);
+    this.vel = new Vec;
+  }
+}
+
+const ball = new Ball;
+console.log(ball);
 
 //we have a black rectangle canvas
 const canvas = document.getElementById('pong');
